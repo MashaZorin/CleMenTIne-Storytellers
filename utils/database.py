ddac57fa193c770ff02fb559ef85724859251794
Tiffany Chen <tiffanysta.c@gmail.com>
2017-10-26 13:44:22 -0400

    changed function stuff

--- utils/database.py
+++ utils/database.py
@@ -1,18 +1,15 @@
 #Database stuff - Tiffany
 
 import sqlite3   #enable control of an sqlite database
-import csv       #facilitates CSV I/O
 
 f="../data/dummy.db"
 
 db = sqlite3.connect(f) #open if f exists, otherwise create
 c = db.cursor()    #facilitate db ops
 
-
-
 # FUNCTIONS!!!
 
-def checkUsernames(username):
+def checkUsernames(username, c):
     #checks if username is taken
     #called when user registers onto our site
     #returns True if username is taken, returns False if username is not taken
@@ -22,7 +19,17 @@ def checkUsernames(username):
             return True
     return False
 
-def updateUsers(username, password): 
+def checkTitles(title, c):
+    #checks if username is taken
+    #called when user registers onto our site
+    #returns True if username is taken, returns False if username is not taken
+    cm = "SELECT title FROM log;"
+    for i in c.execute(cm):
+        if title == i[0].encode("ascii"):
+            return True
+    return False
+
+def updateUsers(username, password, c): 
     #adds a new record in the userInfo table with the username, password, and a userID (found in function)
     #called when user registers onto our site
     cm = "SELECT COUNT(*) FROM userInfo;"
@@ -31,7 +38,7 @@ def updateUsers(username, password):
     cm = 'INSERT INTO userInfo VALUES("%s", "%s", %d)' %(username, password, userID)
     c.execute(cm)
 
-def authorize(username, password):
+def authorize(username, password, c):
     #checks whether a person's password matches their username
     #called by authorize() in app.py
     cm = 'SELECT password FROM userInfo WHERE user = "%s";' %username
@@ -40,11 +47,13 @@ def authorize(username, password):
         true_pass = i
     return password == true_pass[0].encode("ascii")
 
-def addLine(line, story_title, userid):
+def addLine(line, story_title, userid, c):
     #adds a line into a story
     #called when a user submits an edit to a story
-    story_id = getStoryID(story_title)
-    cm = 'UPDATE edited SET '
+    storyid = getStoryID(story_title)
+    cm = 'INSERT INTO edited VALUES (%d, %d)' %(userid, storyid)
+    c.execute(cm)
+    cm = 'UPDATE '
 
 # END FUNCTIONS
 
@@ -52,19 +61,19 @@ def addLine(line, story_title, userid):
 
 # START ALL OUR GET FUNCTIONS
 
-def getUserID(username):
+def getUserID(username, c):
     cm = 'SELECT id FROM userInfo WHERE user = "%s";' %username
     for i in c.execute(cm):
         x = i[0]
     return x
 
-def getStoryID(title):
-    cm = 'SELECT story_id FROM log WHERE title = "%s";' %title
+def getStoryID(title, c):
+    cm = 'SELECT storyId FROM log WHERE title = "%s";' %title
     for i in c.execute(cm):
         x = i[0]
     return x
 
-def getStory(title):
+def getStory(title, c):
     cm = 'SELECT body FROM log WHERE title = "%s";' %title
     for i in c.execute(cm):
         x = i[0].encode("ascii")
@@ -77,14 +86,20 @@ def getStory(title):
 # START ALL OUR TESTS
 
 # updateUsers('"debra"', '"itsstorytime"') -> function works
-print authorize('alice', 'pass') # should print true
-print authorize('alice', 'pas') # should print false
-print checkUsernames('alice') # should print true
-print checkUsernames('eliza') # should print false
-print getUserID('alice')
-print getUserID('bob')
-print getUserID('charlie')
-print getUserID('debra')
+print authorize('alice', 'pass', c) # should print true
+print authorize('alice', 'pas', c) # should print false
+print checkUsernames('alice', c) # should print true
+print checkUsernames('eliza', c) # should print false
+print checkTitles('DW', c) # should print true
+print checkTitles('wrong', c) # should print false
+print getUserID('alice', c)
+print getUserID('bob', c)
+print getUserID('charlie', c)
+print getUserID('debra', c)
+print getStoryID("Tail", c)
+print getStoryID("DW", c)
+print getStory("Tail", c)
+print getStory("DW", c)
 
 # END ALL OUR TESTS
 
